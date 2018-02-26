f4b8a541646e42b581b0965022e0cfd4cc114398
AlisonGonzalez <alisongonzalez@outlook.com>
2018-02-25 19:31:41 -0600

    Funcionalidad de streaming

--- app/src/main/java/alisongonzalez/conceptoradial/MainActivity.java
+++ app/src/main/java/alisongonzalez/conceptoradial/MainActivity.java
@@ -1,16 +1,24 @@
 package alisongonzalez.conceptoradial;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-
     private TextView mTextMessage;
-
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -39,6 +47,27 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        playButton = (Button) findViewById(R.id.play);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource("http://204.12.193.98:8139/");
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                    playButton.setText("Iniciar Streaming");
+                } else {
+                    mediaPlayer.start();
+                    playButton.setText("Pausar Streaming");
+                }
+            }
+        });
+    }
 }
