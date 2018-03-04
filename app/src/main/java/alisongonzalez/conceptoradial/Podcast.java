67a77b3e5e258496f880058eeecacaa02bd3f750
AlisonGonzalez <alisongonzalez@outlook.com>
2018-03-03 19:55:35 -0600

    Lista de podcasts con JSON

---
+++ app/src/main/java/alisongonzalez/conceptoradial/Podcast.java
@@ -0,0 +1,10 @@
+package alisongonzalez.conceptoradial;
+
+/**
+ * Created by AlisonGonzalez on 03/03/18.
+ */
+
+public class Podcast {
+    public String titulo, URL;
+}
+
--- app/src/main/java/alisongonzalez/conceptoradial/PodcastsFragment.java
+++ app/src/main/java/alisongonzalez/conceptoradial/PodcastsFragment.java
@@ -6,13 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PodcastsFragment extends Fragment {
-
+    private String podcastsJson;
 
     public PodcastsFragment() {
         // Required empty public constructor
@@ -23,7 +34,47 @@ public class PodcastsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_podcasts, container, false);
+
+        podcastsJson = loadJSONFromAsset();
+        final ListView listView = (ListView) view.findViewById(R.id.podcastList);
+        final ArrayList<String> arrayList = new ArrayList<>();
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this.getContext(),
+                android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        try{
+            JSONObject jsonObject = new JSONObject(podcastsJson);
+            JSONArray jsonArray = jsonObject.getJSONArray("Medio Equis");
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject podcast = jsonArray.getJSONObject(i);
+                String podcastString = podcast.optString("titulo");
+                if (podcastString != null){
+                    arrayList.add(podcastString);
+                }
+            }
+            arrayAdapter.notifyDataSetChanged();
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
         return view;
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getActivity().getAssets().open("podcasts.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 
 }
